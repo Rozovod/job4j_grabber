57545063ba00b74ae1d90b920ef7285c56475200
Rozovod <rozovod.store@mail.ru>
2022-07-13 13:39:03 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -13,18 +13,21 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            Element dataElement = row.select(".vacancy-card__date").first();
-            Element timeElement = dataElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            String dateTime = timeElement.attr("datetime");
-            System.out.printf("%s %s %s%n", vacancyName, link, dateTime);
-        });
+        for (int i = 1; i < 6; i++) {
+            String pageNumb = String.format("%s?page=%s", PAGE_LINK, i);
+            Connection connection = Jsoup.connect(pageNumb);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                Element dataElement = row.select(".vacancy-card__date").first();
+                Element timeElement = dataElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                String dateTime = timeElement.attr("datetime");
+                System.out.printf("%s %s %s%n", vacancyName, link, dateTime);
+            });
+        }
     }
 }
