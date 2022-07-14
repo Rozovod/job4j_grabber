f6c5c887ed3ca36f2feebddb177e9488bb6eef76
Rozovod <rozovod.store@mail.ru>
2022-07-14 15:13:58 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,12 +5,21 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     private String retrieveDescription(String link) throws IOException {
         Connection connection = Jsoup.connect(link);
@@ -21,21 +30,34 @@ public class HabrCareerParse {
     }
 
     public static void main(String[] args) throws IOException {
+        HabrCareerDateTimeParser timeParser = new HabrCareerDateTimeParser();
+        HabrCareerParse parse = new HabrCareerParse(timeParser);
+        List<Post> posts = parse.list(PAGE_LINK);
+        for (Post post : posts) {
+            System.out.println(post);
+        }
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> posts = new ArrayList<>();
         for (int i = 1; i < 6; i++) {
-            String pageNumb = String.format("%s?page=%s", PAGE_LINK, i);
+            String pageNumb = String.format("%s?page=%s", link, i);
             Connection connection = Jsoup.connect(pageNumb);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
+            for (Element row : rows) {
                 Element titleElement = row.select(".vacancy-card__title").first();
                 Element linkElement = titleElement.child(0);
                 Element dataElement = row.select(".vacancy-card__date").first();
                 Element timeElement = dataElement.child(0);
                 String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                String linkVacancy = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 String dateTime = timeElement.attr("datetime");
-                System.out.printf("%s %s %s%n", vacancyName, link, dateTime);
-            });
+                posts.add(new Post(vacancyName, linkVacancy,
+                        retrieveDescription(linkVacancy), dateTimeParser.parse(dateTime)));
+            }
         }
+        return posts;
     }
 }
