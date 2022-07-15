9680107d4884fe939e84ae70b10bb6d999bc8a2b
Rozovod <rozovod.store@mail.ru>
2022-07-15 16:22:24 +0300

    6. Grabber. [#289477 #322470]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -14,6 +14,7 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 public class Grabber implements Grab {
     private final Properties cfg = new Properties();
+    private static final String LINK = "https://career.habr.com/vacancies/java_developer";
 
     public Store store() {
         return new PsqlStore(cfg);
@@ -56,7 +57,7 @@ public class Grabber implements Grab {
             JobDataMap map = context.getJobDetail().getJobDataMap();
             Store store = (Store) map.get("store");
             Parse parse = (Parse) map.get("parse");
-            List<Post> posts = parse.list("https://career.habr.com/vacancies/java_developer");
+            List<Post> posts = parse.list(LINK);
             for (Post post : posts) {
                 store.save(post);
             }
